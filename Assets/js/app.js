6d5ae6c89648d7042a873d99b6dcdd68a20666fc
Charles <charlescoquerelle.1993@gmail.com>
2023-10-27 09:15:24 +0200

    Modal start

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,13 +1,24 @@
+var modal = document.getElementById("modalCss");
 
-document.getElementById('openContactWindow').addEventListener('click', function() {
-    document.querySelector('.contact-window').style.display = 'block';
-});
+// Get the button that opens the modal
+var btn = document.getElementById("btnCss");
 
-document.querySelector('.contact-window').addEventListener('click', function(e) {
-    if (e.target === this) {
-        this.style.display = 'none';
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-});
-
-
-// VOIR PLUS
+}
